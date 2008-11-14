ddbd57f7b3c8e593b6410c859e29c2d7d00f67ca
nuno@ufp.edu.pt <nuno@ufp.edu.pt@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-11-14 11:23:04 +0000

    r54973 | nuno@ufp.edu.pt | 2008-11-14 09:27:35 +0000 (Fri, 14 Nov 2008) | 1 line
    STAT-99: Ability to report on anonymous access to files
    
    
    git-svn-id: https://source.sakaiproject.org/svn/sitestats/ufp/sitestats/branches/sitestats_2.5.x@71984 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sitestats-tool/src/java/org/sakaiproject/sitestats/tool/jsf/UserIdEidConverter.java
+++ sitestats-tool/src/java/org/sakaiproject/sitestats/tool/jsf/UserIdEidConverter.java
@@ -6,27 +6,31 @@ import javax.faces.convert.CharacterConverter;
 
 import org.sakaiproject.user.api.UserNotDefinedException;
 import org.sakaiproject.user.cover.UserDirectoryService;
+import org.sakaiproject.util.ResourceLoader;
 
 
 public class UserIdEidConverter extends CharacterConverter {
+	/** Resource Bundle */
+	private String 			bundleName 	= FacesContext.getCurrentInstance().getApplication().getMessageBundle();
+	private ResourceLoader	msgs		= new ResourceLoader(bundleName);
 
 	public String getAsString(FacesContext context, UIComponent component, Object value) {
 		String userEid = null;
-		if(value == null){
-			userEid = "-";
-		}else{
-			if(value instanceof String){
-				String userId = (String) value;
+		if (value != null && value instanceof String) {
+      String userId = (String) value;
+			if(("-").equals(userId) || ("?").equals(userId)) {
+				userEid = "-";
+			}else{
 				try{
 					userEid = UserDirectoryService.getUser(userId).getDisplayId();
 				}catch(UserNotDefinedException e1){
 					userEid = userId;
 				}
 			}
-			userEid = super.getAsString(context, component, (Object) userEid);
+		}else{
+			userEid = msgs.getString("user_unknown");
 		}
-
-		return userEid;
+    return super.getAsString(context, component, (Object) userEid);
 	}
 
 }
--- sitestats-tool/src/java/org/sakaiproject/sitestats/tool/jsf/UserIdNameConverter.java
+++ sitestats-tool/src/java/org/sakaiproject/sitestats/tool/jsf/UserIdNameConverter.java
@@ -6,25 +6,32 @@ import javax.faces.convert.CharacterConverter;
 
 import org.sakaiproject.user.api.UserNotDefinedException;
 import org.sakaiproject.user.cover.UserDirectoryService;
+import org.sakaiproject.util.ResourceLoader;
 
 public class UserIdNameConverter extends CharacterConverter {
+	/** Resource Bundle */
+	private String 			bundleName 	= FacesContext.getCurrentInstance().getApplication().getMessageBundle();
+	private ResourceLoader	msgs		= new ResourceLoader(bundleName);
 	
 	public String getAsString(FacesContext context, UIComponent component, Object value) {
 		String name = null;
-		if (value == null) {
-			name = "-";
-		} else {
-			if (value instanceof String) {
+		if (value != null && value instanceof String) {
+      String userId = (String) value;
+			if(("-").equals(userId)) {      
+				name = msgs.getString("user_anonymous");
+			}else if(("?").equals(userId)) {
+        name = msgs.getString("user_anonymous_access");
+			}else{
 				try{
-					name = UserDirectoryService.getUser((String)value).getDisplayName();
+					name = UserDirectoryService.getUser(userId).getDisplayName();
 				}catch(UserNotDefinedException e1){
-					name = "-";
+          name = msgs.getString("user_unknown");
 				}
 			}
-			name = super.getAsString(context, component, (Object)name);
+		}else{
+			name = msgs.getString("user_unknown");
 		}
-
-		return name;
+    return super.getAsString(context, component, (Object)name);
 	}
 
 }
