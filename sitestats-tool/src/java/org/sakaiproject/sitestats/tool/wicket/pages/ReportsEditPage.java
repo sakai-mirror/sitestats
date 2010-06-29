c9f7a91c5c751ca29ac37c14296659863d8805ab
nuno@ufp.edu.pt <nuno@ufp.edu.pt@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-06-29 09:58:55 +0000

    ------------------------------------------------------------------------ r78867 | nuno@ufp.edu.pt | 2010-06-29 10:44:45 +0100 (Tue, 29 Jun 2010) | 1 line
    STAT-246: Clicking 'Back' on report edit page (after generating a report) keep changes in report cached object
    ------------------------------------------------------------------------
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/sitestats/branches/sitestats-2.1.x@78868 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sitestats-tool/src/java/org/sakaiproject/sitestats/tool/wicket/pages/ReportsEditPage.java
+++ sitestats-tool/src/java/org/sakaiproject/sitestats/tool/wicket/pages/ReportsEditPage.java
@@ -301,6 +301,7 @@ public class ReportsEditPage extends BasePage {
 		final Button back = new Button("back") {
 			@Override
 			public void onSubmit() {
+				reportDefModel.detach();
 				setResponsePage(returnPage);
 				super.onSubmit();
 			}
